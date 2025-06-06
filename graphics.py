ccf6cc253b10ffe24b2913de5b792b076f9328bd
Janozeli <lucasmjanozeli@gmail.com>
2025-06-06 13:10:12 -0300

    Draw Lines

--- graphics.py
+++ graphics.py
@@ -1,10 +1,14 @@
 from tkinter import Tk, BOTH, Canvas
+from line import Line
+
 
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas = Canvas(
+            self.__root, bg="white", width=width, height=height
+        )
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -21,3 +25,6 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from graphics import Window
+from point import Point
+from line import Line
+
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(0, 0), Point(800, 600)), "red")
+    win.draw_line(Line(Point(800, 0), Point(0, 600)), "red")
     win.wait_for_close()
+
 
 main()
